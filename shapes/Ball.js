cf7752f946d8a8f8ac4f29e9b992c79e8770b4d5
buglas <liwei1051904257@gmail.com>
2019-11-08 09:51:10 +0800

    sec

---
+++ shapes/Ball.js
@@ -0,0 +1,10 @@
+export default class Ball{
+    constructor(width,height,color){
+        this.width=width;
+        this.height=height;
+        this.color=color;
+    }
+    draw(ctx){
+
+    }
+}
